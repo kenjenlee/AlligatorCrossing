4fbbbd325f0109609e23afb709d15d868edf365b
leekenjen2328 <kj24lee@uwaterloo.ca>
2016-12-25 15:52:57 -0500

    Initial commit

---
+++ android/src/com/magicians/reflectorcrazy/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.magicians.reflectorcrazy;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.magicians.reflectorcrazy.Reflector;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Reflector(), config);
+	}
+}
---
+++ desktop/src/com/magicians/reflectorcrazy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.magicians.reflectorcrazy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.magicians.reflectorcrazy.Reflector;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Reflector(), config);
+	}
+}
