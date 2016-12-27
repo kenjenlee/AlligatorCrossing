3c2e90c330b615648ff8e5e8ad319f0519080aeb
leekenjen2328 <kj24lee@uwaterloo.ca>
2016-12-26 21:20:33 -0500

    Update: 1) Partial Collision Detection 2) Scoring system 3) Made game more playable by managing gaps and difference in distance of consecutive gaps
    Next Update:
    1) Displaying information (score, highscore...)
    2) Complete Collision Detection

--- core/src/com/magicians/reflectorcrazy/Reflector.java
+++ core/src/com/magicians/reflectorcrazy/Reflector.java
@@ -7,29 +7,38 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 //import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 //import com.magicians.reflectorcrazy.InputHandler.InputHandler;
-
 import java.util.Random;
-
 import static java.lang.Math.abs;
 //import com.magicians.reflectorcrazy.GameManager.GameManager;
 
 
 
 public class Reflector extends ApplicationAdapter {
-	private SpriteBatch batch;
+
+    private SpriteBatch batch;
 	private OrthographicCamera camera;
 	//viewport height and width
-
 	private float viewportWidth, viewportHeight;
     private int gameState = 0;
     private Vector3 touchCoord;
-    private float ballX, ballY;
     private int score = 0;
-    private float time = 0;
-    private float ballVelocity = 900;
 
+    //shape Renderer
+    private ShapeRenderer shapeRenderer;
+    private Rectangle[] rectLeft;
+    private Rectangle[] rectRight;
+    private Circle circleBall;
+
+    // ball variables
+    private float ballVelocity = 900;
+    private float ballX, ballY;
+
+    //pillar variables
     private float pillarVelocity = 3;
     private Random  randomNum;
     private float[] pillarOffset;
@@ -38,10 +47,14 @@ public class Reflector extends ApplicationAdapter {
     //pillarDist is distance between bottom left corner of
     //two consecutive pillars
     private float pillarDist;
-    private int pillarNum = 10;
+    private int pillarNum = 7;
     //gap between pillars in same row
     private float gap;
+    private int pillarScoring = 0;
+    private int maxConsecGapDist = 700;
+    private int minConsecGapDist = 200;
 
+    // Textures
     private Texture ball;
     private Texture pillarLeft;
     private Texture pillarRight;
@@ -56,35 +69,51 @@ public class Reflector extends ApplicationAdapter {
 		viewportWidth = Gdx.graphics.getWidth();
         //set camera dimension to screen dimension
 		camera = new OrthographicCamera(viewportWidth, viewportHeight);
+        touchCoord = new Vector3();
 
+        //Texture variables
         ball = new Texture(Gdx.files.internal("badlogic.jpg"));
         pillarLeft = new Texture(Gdx.files.internal("pillarLeft.png"));
         pillarRight = new Texture(Gdx.files.internal("pillarRight.png"));
      //   background = new Texture(Gdx.files.internal(""));
 
-
-        gap = ball.getWidth() * 1.5f;
-        touchCoord = new Vector3();
+        //ball variables
         ballX = Gdx.graphics.getWidth()/2 - ball.getWidth()/2;
         ballY = Gdx.graphics.getHeight()/3 - ball.getHeight()/2;
-        randomNum = new Random();
+
+        //pillar variables
         pillarOffset = new float[pillarNum];
         pillarY = new float[pillarNum];
         pillarDist = Gdx.graphics.getHeight() / 4;
-        maxPillarOffset = 500;
-        // maxPillarOffset = Gdx.graphics.getWidth()/2 - gap/2 - 100;
-       // pillarWidth = pillarLeft.getWidth()
+        maxPillarOffset = Gdx.graphics.getWidth()/2 + gap/2 - 1.5f*ball.getWidth();
+        gap = ball.getWidth() * 1.5f;
+        randomNum = new Random();
+
+        //shapeRenderer variables
+        shapeRenderer = new ShapeRenderer();
+        rectLeft = new Rectangle[pillarNum];
+        rectRight = new Rectangle[pillarNum];
+        circleBall = new Circle();
 
 
         for(int i=0; i<pillarNum; i++){
 
             //ensure gap in rows next to each other is not too far away from each other
+            int previous = i-1;
+            if(i == 0){
+                previous = pillarNum-1;
+            }
             do{
                 pillarOffset[i] = (randomNum.nextFloat()-0.5f)* maxPillarOffset * 2;
-            }while(i>0 && abs(pillarOffset[i]-pillarOffset[i-1])>500);
+            }while(i>0 && abs(pillarOffset[i]-pillarOffset[previous])>maxConsecGapDist &&
+                    abs(pillarOffset[i]-pillarOffset[previous])<minConsecGapDist);
 
             //first pillar start from just off screen to the top
             pillarY[i] = Gdx.graphics.getHeight() + i * pillarDist;
+
+            //shapeRenderer shapes
+            rectLeft[i] = new Rectangle();
+            rectRight[i] = new Rectangle();
         }
 
 	}
@@ -95,41 +124,57 @@ public class Reflector extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
      //   batch.setProjectionMatrix(camera.combined);
 		batch.begin();
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(com.badlogic.gdx.graphics.Color.BLACK);
 
         if(gameState==0 && Gdx.input.justTouched()){
             //start game
             gameState = 1;
         }else if(gameState == 1){
+
+            //Track ball movement
             if(Gdx.input.isTouched()){
 
                 touchCoord.set(Gdx.input.getX(), Gdx.input.getY(),0);
                // camera.unproject(touchCoord);
-
               //  float touchX = touchCoord.x;
 
-                if(Gdx.input.getX() < Gdx.graphics.getWidth()/2){
-                    //ball goes left
+                if(Gdx.input.getX() < ballX+ball.getWidth()/2){
+                    //ball goes left if user touches to the left of ball
                     if(ballX > 0){ //ensure ball does not go off screen
-                        ballX -= ballVelocity; // * Gdx.graphics.getRawDeltaTime();
+                        ballX -= ballVelocity;
                     }
-
-                }else if(Gdx.input.getX() > Gdx.graphics.getWidth()/2){
+                }else {
                     //ball goes right
                     if(ballX + ball.getWidth() < Gdx.graphics.getWidth()){
-                        ballX += ballVelocity; // * Gdx.graphics.getRawDeltaTime();
+                        ballX += ballVelocity;
                     }
                 }
             }
 
+            //Scoring system
+            if(pillarY[pillarScoring] < Gdx.graphics.getHeight()/3){
+                score++;
+                if(pillarScoring < pillarNum -1)
+                    pillarScoring++;
+                else
+                    pillarScoring = 0;
+            }
+
             for(int i=0; i<pillarNum; i++){
-                pillarY[i] -= pillarVelocity; // * Gdx.graphics.getRawDeltaTime();
+                pillarY[i] -= pillarVelocity;
 
                 if(pillarY[i] <= -Gdx.graphics.getHeight()*0.05){
                     pillarY[i] = pillarNum * pillarDist;
                     //set offset to new random offset
+                    int previous = i-1;
+                    if(i == 0){
+                         previous = pillarNum-1;
+                    }
                     do{
                         pillarOffset[i] = (randomNum.nextFloat()-0.5f)* maxPillarOffset * 2;
-                    }while(i>0 && abs(pillarOffset[i]-pillarOffset[i-1])>500);
+                    }while(abs(pillarOffset[i]-pillarOffset[previous])>maxConsecGapDist &&
+                            abs(pillarOffset[i]-pillarOffset[previous])<minConsecGapDist);
                 }
 
                 float pillarLeftLen = Gdx.graphics.getWidth()/2 - gap/2 + pillarOffset[i];
@@ -144,15 +189,11 @@ public class Reflector extends ApplicationAdapter {
             }
 
             batch.draw(ball, ballX, ballY);
+            circleBall.set(ballX+ball.getWidth()/2, ballY+ball.getHeight()/2, ball.getHeight()/2);
+            shapeRenderer.circle(circleBall.x, circleBall.y, circleBall.radius);
 
-           // time += Gdx.graphics.getRawDeltaTime();
-           // if(time%7 == 0){
-                //Increase speed every 7 seconds
-                ballVelocity += 0.0005;
-                pillarVelocity += 0.0005;
-         //   }
-
-
+            ballVelocity += 0.0005;
+            pillarVelocity += 0.0005;
 
         }else{ //game over
             //display score and highest score
@@ -165,17 +206,22 @@ public class Reflector extends ApplicationAdapter {
             pillarVelocity = 3;
             ballVelocity = 3;
             gameState = 0;
+            pillarScoring = 0;
         }
         //GameManager.render(batch, touchCount);
 		batch.end();
+        shapeRenderer.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
+        shapeRenderer.dispose();
         pillarLeft.dispose();
         pillarRight.dispose();
         ball.dispose();
+
       //  background.dispose();
+
 	}
 }
